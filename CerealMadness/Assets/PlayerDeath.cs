06a51f83fe88bb7b3a636a776e4bf6244778cf4f
Danielis Joniskis <g00333859@gmit.ie>
2018-10-09 21:12:23 +0100

    added PlayerDeath script to player

---
+++ CerealMadness/Assets/PlayerDeath.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDeath : MonoBehaviour {
+
+    public int Health;
+
+    // Update is called once per frame
+    void Update(){
+        if (gameObject.transform.position.y < -10){
+            Die();
+        }
+    }
+
+    //Upon "Death" the prototype file is loaded, effectively restarting the game.
+    void Die(){
+        SceneManager.LoadScene(0);
+    }
+}
