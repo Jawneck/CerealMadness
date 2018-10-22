f85df3010f0fe4a0dd70957cc7f072e59596458e
unknown <dananasas@gmail.com>
2018-10-22 23:56:28 +0100

    weapon facing mouse cursor

--- CerealMadness/Assets/Weapon.cs
+++ CerealMadness/Assets/Weapon.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float offset;
+
+    private void Update()
+    {
+        //Calculating the direction between the weapon and the mouse cursor
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        //Calculating the amount of degrees the weapon must rotate to face the cursor
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        //Setting the weapon rotation
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ + offset);
+    }
 }
