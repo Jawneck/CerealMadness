98556d03b1458209d29cf1af31e311bf7b360039
unknown <dananasas@gmail.com>
2018-10-23 08:28:56 +0100

    Rearranged project files

--- CerealMadness/Assets/Scripts/Weapon.cs
+++ CerealMadness/Assets/Scripts/Weapon.cs
@@ -6,13 +6,19 @@ public class Weapon : MonoBehaviour {
 
     public float offset;
 
-    private void Update()
-    {
+    public GameObject projectile;
+    public Transform shootingPoint;
+
+    private void Update(){
         //Calculating the direction between the weapon and the mouse cursor
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         //Calculating the amount of degrees the weapon must rotate to face the cursor
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         //Setting the weapon rotation
         transform.rotation = Quaternion.Euler(0f, 0f, rotationZ + offset);
+
+        if (Input.GetMouseButtonDown(0)){
+            Instantiate(projectile, shootingPoint.position, transform.rotation);
+        }
     }
 }
