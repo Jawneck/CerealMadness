23c77abc7b5feeee58f6451096c7e098b6bf72df
unknown <dananasas@gmail.com>
2018-10-23 09:59:15 +0100

    detecting projectile hitting enemy

--- CerealMadness/Assets/Scripts/Projectile.cs
+++ CerealMadness/Assets/Scripts/Projectile.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 public class Projectile : MonoBehaviour {
 
     public float speed;
+    public float distance;
+    public LayerMask whatIsSolid;
 
-	private void Update () {
+    private void Update () {
+
+        //Detecting if projectile hits enemy
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, distance, whatIsSolid);
+        if(hit.collider != null){
+            if (hit.collider.CompareTag("Enemy")){
+                Debug.Log("Enemy hit");
+            }
+        }
+
         //Moving the projectile forward
         transform.Translate(Vector2.up * speed * Time.deltaTime);
 	}
