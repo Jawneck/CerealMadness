ffada4a3db23eaa37f39fd918604c464d4c70528
unknown <dananasas@gmail.com>
2018-10-23 08:45:37 +0100

    Spawning projectile with time constraint

--- CerealMadness/Assets/Scripts/Weapon.cs
+++ CerealMadness/Assets/Scripts/Weapon.cs
@@ -9,6 +9,9 @@ public class Weapon : MonoBehaviour {
     public GameObject projectile;
     public Transform shootingPoint;
 
+    public float timeBetweenShots;
+    public float startTimeBetweenShots;
+
     private void Update(){
         //Calculating the direction between the weapon and the mouse cursor
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
@@ -17,8 +20,18 @@ public class Weapon : MonoBehaviour {
         //Setting the weapon rotation
         transform.rotation = Quaternion.Euler(0f, 0f, rotationZ + offset);
 
-        if (Input.GetMouseButtonDown(0)){
-            Instantiate(projectile, shootingPoint.position, transform.rotation);
+        if(timeBetweenShots <= 0){
+            //Spawning projectile at the tip of the weapon
+            if (Input.GetMouseButtonDown(0)){
+                Instantiate(projectile, shootingPoint.position, transform.rotation);
+                //Resetting time between shots
+                timeBetweenShots = startTimeBetweenShots;
+            }
         }
+        else{
+            timeBetweenShots -= Time.deltaTime;
+        }
+
+
     }
 }
