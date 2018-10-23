fcd403bcb4c141bb50ba48fc51e7c38105da6aa7
unknown <dananasas@gmail.com>
2018-10-23 08:54:46 +0100

    thrusting projectile from weapon

---
+++ CerealMadness/Assets/Scripts/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed;
+
+	private void Update () {
+        //Moving the projectile forward
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+	}
+}
