32d1f94446645ccb23db233f46d3db5442f4337a
unknown <dananasas@gmail.com>
2018-11-13 00:17:06 +0000

    Loading level 1 with play button

---
+++ CerealMadness/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+
+        //Loads the next level in the queue, in this case index 1, which is level 1.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+
+        //Closes down the program.
+        Application.Quit();
+    }
+}
