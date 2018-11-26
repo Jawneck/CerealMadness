782d4c8a38d6ba5b6bd9dc99723521e138fce166
unknown <dananasas@gmail.com>
2018-11-26 12:31:56 +0000

    hearts showing on canvas

--- CerealMadness/Assets/Scripts/PlayerHealth.cs
+++ CerealMadness/Assets/Scripts/PlayerHealth.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 
-    public int health; 
+    public int health;
+    public int numOfHearts;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    private void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
 }
