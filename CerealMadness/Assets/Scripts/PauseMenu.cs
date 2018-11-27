d781f343b9d54879510247b6067a747c7cfa532c
unknown <dananasas@gmail.com>
2018-11-27 01:36:25 +0000

    started working on pause menu

---
+++ CerealMadness/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    [SerializeField] private GameObject pauseMenuUI;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+        }
+    }
+}
