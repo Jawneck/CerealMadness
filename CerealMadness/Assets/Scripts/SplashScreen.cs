4833446fde1fc88537e74f2b836336b963b3b40c
unknown <dananasas@gmail.com>
2018-11-27 00:40:05 +0000

    splash screen fading in/out

--- CerealMadness/Assets/Scripts/SplashScreen.cs
+++ CerealMadness/Assets/Scripts/SplashScreen.cs
@@ -7,7 +7,6 @@ using UnityEngine.SceneManagement;
 public class SplashScreen : MonoBehaviour {
 
     public Image splashImage;
-    public string loadLevel;
 
     IEnumerator Start()
     {
@@ -17,16 +16,16 @@ public class SplashScreen : MonoBehaviour {
         yield return new WaitForSeconds(2.5f);
         FadeOut();
         yield return new WaitForSeconds(2.5f);
-        SceneManager.LoadScene(loadLevel);
+        SceneManager.LoadScene(1);
     }
 
     void FadeIn()
     {
-
+        splashImage.CrossFadeAlpha(1.0f, 1.5f, false);
     }
 
     void FadeOut()
     {
-
+        splashImage.CrossFadeAlpha(0.0f, 2.5f, false);
     }
 }
