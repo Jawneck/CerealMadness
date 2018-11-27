365512f28aa60ea6cbaff8e750a3303565a3ee9f
unknown <dananasas@gmail.com>
2018-11-27 00:30:52 +0000

    working on splash screen

---
+++ CerealMadness/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+    public Image splashImage;
+    public string loadLevel;
+
+    IEnumerator Start()
+    {
+        splashImage.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+        FadeOut();
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene(loadLevel);
+    }
+
+    void FadeIn()
+    {
+
+    }
+
+    void FadeOut()
+    {
+
+    }
+}
