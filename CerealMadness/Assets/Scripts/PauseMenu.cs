3e2b7f9522aa87a70ef7136d1ef52680c8196131
unknown <dananasas@gmail.com>
2018-11-27 01:42:10 +0000

    activating/deactivating pause menu on keypress

--- CerealMadness/Assets/Scripts/PauseMenu.cs
+++ CerealMadness/Assets/Scripts/PauseMenu.cs
@@ -6,11 +6,34 @@ public class PauseMenu : MonoBehaviour {
 
     [SerializeField] private GameObject pauseMenuUI;
 
+    [SerializeField] private bool isPaused;
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-
+            isPaused = !isPaused;
         }
+
+        if (isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+
+    void ActivateMenu()
+    {
+        pauseMenuUI.SetActive(true);
+
+    }
+
+    void DeactivateMenu()
+    {
+        pauseMenuUI.SetActive(false);
     }
 }
