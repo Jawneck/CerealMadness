b953a3e4a200e06cedbe68cb47c3f588ee1d080d
unknown <dananasas@gmail.com>
2018-12-04 13:00:28 +0000

    enemy projectile moving to player

--- CerealMadness/Assets/Scripts/EnemyProjectile.cs
+++ CerealMadness/Assets/Scripts/EnemyProjectile.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class EnemyProjectile : MonoBehaviour {
 
+    public float speed;
+
+    private Transform player;
+    private Vector2 target;
+
 	// Use this for initialization
 	void Start () {
-		
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(player.position.x, player.position.y);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (transform.position.x == target.x && transform.position.y == target.y){
+            DestroyProjectile();
+        }
 	}
+
 }
