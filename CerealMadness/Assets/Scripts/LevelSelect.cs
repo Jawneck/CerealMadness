6de90170702215a6a7fa5eb368f45974239fb417
unknown <dananasas@gmail.com>
2018-12-05 19:21:10 +0000

    added level select

---
+++ CerealMadness/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour {
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+}
--- CerealMadness/Assets/Scripts/MainMenu.cs
+++ CerealMadness/Assets/Scripts/MainMenu.cs
@@ -8,14 +8,12 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-
         //Loads the next level in the queue, in this case index 1, which is level 1.
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
-
         //Closes down the program.
         Application.Quit();
     }
